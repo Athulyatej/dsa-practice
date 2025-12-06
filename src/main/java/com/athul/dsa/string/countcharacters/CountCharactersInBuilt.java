d5363b0d8ba9b088afa2fd56ef2a73c250923afe
Athulyatej K V <31559594+Athulyatej@users.noreply.github.com>
2025-12-06 22:05:48 +0530

    Character Arrays/Strings Updated (#4)

--- src/main/java/com/athul/dsa/string/countcharacters/CountCharactersInBuilt.java
+++ src/main/java/com/athul/dsa/string/countcharacters/CountCharactersInBuilt.java
@@ -1,8 +1,8 @@
-package com.athul.dsa.string.countCharcters;
+package com.athul.dsa.string.countcharacters;
 
 import java.util.Scanner;
 
-public class CountCharacters {
+public class CountCharactersInBuilt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the string: ");
@@ -14,9 +14,9 @@ public class CountCharacters {
         int alpha = 0, digit = 0, space = 0;
         for (int i = 0; i < sentence.length(); i++) {
             char ch = sentence.charAt(i);
-            if (ch >= '0' && ch <= '9') digit++;
-            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) alpha++;
-            else if (ch == ' ' || ch == '\t') space++;
+            if (Character.isDigit(ch)) digit++;
+            else if (Character.isAlphabetic(ch)) alpha++;
+            else if (Character.isSpaceChar(ch)) space++;
         }
         System.out.println("Alphabets: " + alpha);
         System.out.println("Digits: " + digit);
--- src/main/java/com/athul/dsa/string/removeduplicates/RemoveDuplicates.java
+++ src/main/java/com/athul/dsa/string/removeduplicates/RemoveDuplicates.java
@@ -1,8 +1,8 @@
 package com.athul.dsa.string.removeduplicates;
 
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class RemoveDuplicates {
 
     private static String removeDuplicates(String str) {
         String output = "";
-        Set<Character> set = new HashSet<>();
+        Set<Character> set = new TreeSet<>();
         for (int i = 0; i < str.length(); i++) {
             set.add(str.charAt(i));
         }
